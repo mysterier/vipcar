1e8e99bfc954e0f6b9a1f3f019d9a20e14e58899
leon.lv <leon.lv@vip-car.com.cn>
2015-01-22 17:34:45 +0800

    完成充值成功支付宝回调

--- api/protected/modules/client/controllers/RechargeController.php
+++ api/protected/modules/client/controllers/RechargeController.php
@@ -5,9 +5,9 @@ class RechargeController extends Controller
 
     public function actionIndex()
     {
-        require_once(COMMON . "/alipay/alipay.config.php");
-        require_once(COMMON . "/alipay/lib/alipay_core.function.php");
-        require_once(COMMON . "/alipay/lib/alipay_rsa.function.php");
+        require_once (COMMON . "/alipay/alipay.config.php");
+        require_once (COMMON . "/alipay/lib/alipay_core.function.php");
+        require_once (COMMON . "/alipay/lib/alipay_rsa.function.php");
         $recharge_no = 'rcg' . time();
         $total_fee = $this->getParam('total_fee');
         $subject = $this->getParam('subject');
@@ -41,8 +41,9 @@ class RechargeController extends Controller
             $this->result['pay_info'] = $pay_info;
         }
     }
-    
-    public function actionList() {
+
+    public function actionList()
+    {
         $criteria = new CDbCriteria();
         $criteria->condition = 'uid = :uid and id > :id';
         $criteria->order = 'id asc';
@@ -73,21 +74,72 @@ class RechargeController extends Controller
             $this->result['rechargelist'] = [];
         }
     }
-    
-    public function actionNotify() {
-        require_once(COMMON . "/alipay/alipay.config.php");
-        require_once(COMMON . "/alipay/lib/alipay_notify.class.php");
+
+    public function actionNotify()
+    {
+        require_once (COMMON . "/alipay/alipay.config.php");
+        require_once (COMMON . "/alipay/lib/alipay_notify.class.php");
         $alipayNotify = new AlipayNotify($alipay_config);
-        var_dump($alipayNotify);
-    }
-    
-    private function getLink($para) {
-        $arg  = "";
-        while (list ($key, $val) = each ($para)) {
-            $arg.=$key."=\"".$val."\"&";
+        $verify_result = $alipayNotify->verifyNotify();
+        if ($verify_result) {
+            // 交易状态
+            $trade_status = $this->getParam('trade_status');
+            
+            if ($trade_status !== 'TRADE_FINISHED') {
+                $this->common();
+            } elseif ($trade_status == 'TRADE_SUCCESS') {
+                $this->common();
+            }   
+        } else {
+            // 验证失败
+            echo "fail";
         }
-        //去掉最后一个&字符
-        $arg = substr($arg,0,count($arg)-2);
+        Yii::app()->end();
+    }
+
+    private function common()
+    {
+        // 商户订单号
+        $out_trade_no = $this->getParam('out_trade_no');        
+        // 支付宝交易号        
+        $trade_no = $this->getParam('trade_no');
+        
+        $recharge_obj = RechargeLog::model()->findByAttributes(['recharge_no' => $out_trade_no]);
+        if ($recharge_obj) {
+            if ($recharge_obj->status == 1) {
+                echo "success";
+            } else {
+                $recharge_obj->status = 1;
+                $recharge_obj->trade_no = $trade_no;
+            
+                $transaction = Yii::app()->db->beginTransaction();
+                if ($recharge_obj->save()) {
+                    $model = Clients::model()->findByPk($recharge_obj->uid);
+                    $model->setScenario('update_balance');
+                    $model->account_balance = $model->account_balance + $recharge_obj->amount;
+                    $model->last_update = time();
+                    if ($model->save()) {
+                        $transaction->commit();
+                        echo 'success';
+                    } else {
+                        $transaction->rollback();
+                        echo 'fail';
+                    }
+                }
+            }
+        } else {
+            echo 'fail';
+        }             
+    }
+
+    private function getLink($para)
+    {
+        $arg = "";
+        while (list ($key, $val) = each($para)) {
+            $arg .= $key . "=\"" . $val . "\"&";
+        }
+        // 去掉最后一个&字符
+        $arg = substr($arg, 0, count($arg) - 2);
         
         return $arg;
     }

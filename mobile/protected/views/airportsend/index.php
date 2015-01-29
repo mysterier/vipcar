6743a92d7ce8ec3b4eb5586c2e8f2e3196267c89
leon.lv <leon.lv@vip-car.com.cn>
2015-01-29 18:07:57 +0800

    送机样式

--- mobile/protected/views/airportsend/index.php
+++ mobile/protected/views/airportsend/index.php
@@ -1,83 +1,89 @@
 <div class="container-fluid">
-<div style="margin-top:1em;"class="form-group">
-    <div class="input-group">
-        <div class="input-group-addon iconfont">&#xf0053;</div>
-        <input type="text" class="form-control" id="user_j" placeholder="联系人">
-    </div>
-</div>
+	<div style="margin-top: 1em;" class="form-group">
+		<div class="input-group">
+			<div class="input-group-addon iconfont">&#xf0053;</div>
+			<input type="text" class="form-control" id="user" placeholder="联系人">
+		</div>
+	</div>
 
-<div class="form-group">
-    <div class="input-group">
-        <div class="input-group-addon iconfont">&#xe64a;</div>
-        <input type="tel" class="form-control" id="tel_j" placeholder="联系电话">
-    </div>
-</div>
-
-<div class="form-group">
-    <div class="input-group">
-        <div class="input-group-addon iconfont">&#xe630;</div>
-      <input style="width:70%; float:left;" type="text" class="form-control" id="hangban_j" placeholder="航班号"><button type="button" style="float:right; width:29%;" class="btn btn-primary">查询</button>
-
-    </div>
-</div>
-
-<div class="form-group">
-    <div class="input-group">
-        <div class="input-group-addon iconfont">&#xe738;</div>
-      <input type="datetime" class="form-control" disabled id="timeup_j" placeholder="上车时间">
-    </div>
-</div>
-
-<div class="form-group">
-    <div class="input-group">
-        <div class="input-group-addon iconfont">&#xe622;</div>
-      <input type="text" class="form-control" disabled id="hangzhanlou_j" placeholder="航站楼">
-    </div>
-</div>
-
-
-<div class="form-group">
-    <div class="input-group">
-        <div class="input-group-addon iconfont">&#xe606;</div>
-      <input type="text" class="form-control" id="addressdown_j" placeholder="下车地点">
-    </div>
-</div>
-
- <div class="form-group">
-
-    <div class="input-group">
-     
-        <div class="input-group-addon iconfont">&#xe603;</div>
-        <select class="form-control" >
-        <optgroup label="车型选择"></optgroup>
-       
-          <option>舒适性</option>
-          <option>商务型</option>
-          <option>豪华型</option>
-       </select>
-    </div>
-    </div>
-  
-  
-  
-     <textarea style="width:100%;" class="form-group" rows="3" placeholder="其他需求"></textarea>
-  
-  <label style=" float:right;">价格<span class="rmb">0</span>元</label>  
-  
-<div style="clear:both;" class="form-group">
-	<div class="checkbox">
-      <label>
-        <input type="checkbox">
-        勾选同意<a href="#">《众择用车》</a>协议 </label>
-    </div>
-</div> 
+	<div class="form-group">
+		<div class="input-group">
+			<div class="input-group-addon iconfont">&#xe64a;</div>
+			<input type="tel" class="form-control" id="tel" placeholder="联系电话">
+		</div>
+	</div>
 
 
 
- 
+	<div class="form-group">
+		<div class="input-group">
+			<div class="input-group-addon iconfont">&#xe738;</div>
+			<input type="datetime" class="form-control" id="timeup"
+				placeholder="上车时间">
+		</div>
+	</div>
 
-  
+	<div class="form-group">
+		<div class="input-group">
+			<div class="input-group-addon iconfont">&#xe606;</div>
+			<input type="text" class="form-control" id="addressup"
+				placeholder="上车地点">
+		</div>
+	</div>
 
-<button type="button"  class="btn btn-primary btn-block">提交订单</button>
-</form>
+	<div class="form-group">
+		<div class="input-group">
+			<div class="input-group-addon iconfont">&#xe622;</div>
+			<select class="form-control">
+				<optgroup label="航站楼"></optgroup>
+				<option>一号楼</option>
+				<option>二号楼</option>
+				<option>三号楼</option>
+				<option>四号楼</option>
+				<option>五号楼</option>
+			</select>
+		</div>
+	</div>
+
+
+
+	<div class="form-group">
+
+		<div class="input-group">
+
+			<div class="input-group-addon iconfont">&#xe603;</div>
+			<select class="form-control">
+				<optgroup label="车型选择"></optgroup>
+				<option>舒适性</option>
+				<option>商务型</option>
+				<option>豪华型</option>
+			</select>
+		</div>
+	</div>
+
+	<div class="form-group">
+		<label for="exampleInputFile">是否往返</label>
+		<div style="width: 100%;" class="input-group">
+
+
+			<select class="form-control">
+
+				<option>是</option>
+				<option>否</option>
+
+			</select>
+		</div>
+	</div>
+	<textarea style="width: 100%;" class="form-group" rows="3"
+		placeholder="其他需求"></textarea>
+
+	<label style="float: right;">价格<span class="rmb">0</span>元
+	</label>
+	<div style="clear: both;" class="checkbox">
+		<label> <input type="checkbox"> 勾选同意<a href="#">《众择用车》</a>协议
+		</label>
+	</div>
+
+	<button type="button" class="btn btn-primary btn-block">提交订单</button>
+	</form>
 </div>

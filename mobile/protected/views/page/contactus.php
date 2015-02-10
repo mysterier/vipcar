332b8377014f08f0087c4d8fd10abca74fffbab0
leon.lv <leon.lv@vip-car.com.cn>
2015-02-10 18:00:27 +0800

    加上海市

--- mobile/protected/views/page/contactus.php
+++ mobile/protected/views/page/contactus.php
@@ -1,4 +1,4 @@
 <p class="text-success">联系电话</p>
 <P class="text-muted">400-965-2886</p>
 <p class="text-success">联系地址</p>
-<P class="text-muted">徐汇区宜山路705号A座18楼</p>
+<P class="text-muted">上海市徐汇区宜山路705号A座18楼</p>

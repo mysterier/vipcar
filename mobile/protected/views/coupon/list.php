703fe343a26486e925fb169bab17bcc24dfd76f8
leon.lv <leon.lv@vip-car.com.cn>
2015-02-27 11:12:25 +0800

    coupon logo fix2

--- mobile/protected/views/coupon/list.php
+++ mobile/protected/views/coupon/list.php
@@ -2,7 +2,7 @@
 <div class="bgcolor text-right">
 	<div>
 		<p class="logo-left">
-			<img src="/img/<?php echo $coupon->value;?>.png" width="50" height="50">
+			<img src="/img/50.png" width="50" height="50">
 		</p>
 		<strong><?php echo $coupon->value;?>元优惠券</strong>
 	</div>

3cfb9134293eb979a53e47522035674705ffc57a
leon.lv <leon.lv@vip-car.com.cn>
2015-05-15 14:26:10 +0800

    修改banner

--- www/protected/views/home/_banner.php
+++ www/protected/views/home/_banner.php
@@ -1,18 +1,18 @@
 <div class="banner">
 	<div class="imglist banner-one">
-		<a href="http://www.baidu.com"><img src="/images/banner/banner1.jpg" /></a>
+		<a href="http://www.baidu.com"><img src="/images/index/banner1.jpg" /></a>
 	</div>
-	<div class="imglist banner-two">
-		<img src="/images/banner/banner2.jpg" />
-	</div>
-	<div class="imglist banner-three">
-		<img src="/images/banner/banner3.jpg" />
-	</div>
+<!-- 	<div class="imglist banner-two"> -->
+<!-- 		<img src="/images/banner/banner2.jpg" /> -->
+<!-- 	</div> -->
+<!-- 	<div class="imglist banner-three"> -->
+<!-- 		<img src="/images/banner/banner3.jpg" /> -->
+<!-- 	</div> -->
 
-	<ul class="banner-btn">
-		<li class="banner-hover" dl="banner1"></li>
-		<li dl="banner2"></li>
-		<li dl="banner3"></li>
-	</ul>
+<!-- 	<ul class="banner-btn"> -->
+<!-- 		<li class="banner-hover" dl="banner1"></li> -->
+<!-- 		<li dl="banner2"></li> -->
+<!-- 		<li dl="banner3"></li> -->
+<!-- 	</ul> -->
 	<div style="clear: both"></div>
 </div>

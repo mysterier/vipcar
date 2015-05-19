dfb86462296acd415beedb96fa6ff69f52bf9d38
leon.lv <leon.lv@vip-car.com.cn>
2015-05-19 15:24:58 +0800

    service banner 修改

--- www/protected/views/home/service.php
+++ www/protected/views/home/service.php
@@ -1,5 +1,22 @@
 <!--banner-->
-<?php include __DIR__ . '/../home/_banner.php';?>
+<div class="banner">
+	<div class="imglist banner-one">
+		<a href="/order/pickup"><img src="/images/service/banner1.jpg" /></a>
+	</div>
+	<div class="imglist banner-two">
+		<img src="/images/service/banner2.jpg" />
+	</div>
+	<div class="imglist banner-three">
+		<img src="/images/service/banner3.jpg" />
+	</div>
+
+	<ul class="banner-btn">
+		<li class="banner-hover" dl="banner1"></li>
+		<li dl="banner2"></li>
+		<li dl="banner3"></li>
+	</ul>
+	<div style="clear: both"></div>
+</div>
 
 
 <!--互联网时代专车-->
@@ -14,7 +31,7 @@
     <div class="carmonery">
         <div class="carmonerylist">
             <h3 class="text-center">舒适型</h3>
-            <p class="text-center">别克GL8 陆尊等6座车型 等同级车型</p>
+            <p class="text-center">大众帕萨特、丰田凯美瑞、马自达同级别车型</p>
             <img src="/images/service/car1.png" />
             <div class="car-price">
                 <div class="car-circle">

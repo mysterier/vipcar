abd8eee814bccffcf8f16966d809ca74b107f774
leon.lv <leon.lv@vip-car.com.cn>
2015-06-01 11:01:55 +0800

    修改关注众择样式

--- www/protected/views/home/static/attention.php
+++ www/protected/views/home/static/attention.php
@@ -3,11 +3,11 @@
 <div class="wrapper-width">
 	<div class="attention padding50">
 		<h3 class="text-center">关注众择</h3>
-		<p class="attention-index">“众择用车”是以汽车租赁服务为基础，搭载移动互联网和智能用车软件，提供专业机场、高铁接送服务。“众择用车”拥有自主经营的多种车型及专业司机团队，智能化实时订单处理，24小时专人专车服务，让您的出行更轻松、便捷、舒适、安全。
+		<p class="">“众择用车”是以汽车租赁服务为基础，搭载移动互联网和智能用车软件，提供专业机场、高铁接送服务。“众择用车”拥有自主经营的多种车型及专业司机团队，智能化实时订单处理，24小时专人专车服务，让您的出行更轻松、便捷、舒适、安全。
 		</p>
-		<div class="attention-left">
+		<div class="attention-pad-left attention-left">
 			<h4>众择用车官方微信</h4>
-			<div class="col-md-12 attention-img-code">
+			<div class="attention-img-code">
 				<img class="img-border" src="/img/weixing-code.jpg">				
 			</div>
 			<div class="attention-left-txt">
@@ -19,9 +19,9 @@
 				</p>
 			</div>
 		</div>
-		<div class="attention-right">
+		<div class="attention-pad-right attention-right">
 			<img src="/img/weiboicon.png">
-			<div class="col-md-12">
+			<div class="">
 				<img class="img-border"  src="/img/weibo-code.png">
 			</div>
 			<div class="attention-right-txt">
